fdaac93db82cc36c47c24473162be88222801214
SupermercadoEmporium <supermercado.emporium@gmail.com>
2016-01-14 16:48:08 -0430

    Update server.R

--- server.R
+++ server.R
@@ -197,3 +197,104 @@ output$tabla1namecat2Diciembre<-renderText({
   }
   paste( names(t[2]))
 })
+
+output$tabla1probcat2Diciembre<-renderText({
+  Subconjuntos<-na.omit(subset(catg_sin_rep_Diciembre,
+                               Diciembre.Categoriaf3==input$select2,
+                               select=c(Diciembre.CATEGORIA1)))    
+  
+  sub<-Subconjuntos[,1]; tab_sub<-table(sub);
+  tab_sub_order<-sort(tab_sub, decreasing=T)
+  t<-tab_sub_order[1:3]/length(sub)
+  paste(round(t[2], digits=4))
+})
+
+output$tabla1namecat3Diciembre<-renderText({
+  Subconjuntos<-na.omit(subset(catg_sin_rep_Diciembre,
+                               Diciembre.Categoriaf3==input$select2,
+                               select=c(Diciembre.CATEGORIA1)))    
+  
+  sub<-Subconjuntos[,1]; tab_sub<-table(sub);
+  tab_sub_order<-sort(tab_sub, decreasing=T)
+  t<-tab_sub_order[1:3]/length(sub)
+  nam<-vector(mode="character")
+  for(i in 1:3){
+    nam[i]<-names(t[i])
+  }
+  paste( names(t[3]))
+})
+
+output$tabla1probcat3Diciembre<-renderText({
+  Subconjuntos<-na.omit(subset(catg_sin_rep_Diciembre,
+                               Diciembre.Categoriaf3==input$select2,
+                               select=c(Diciembre.CATEGORIA1)))    
+  
+  sub<-Subconjuntos[,1]; tab_sub<-table(sub);
+  tab_sub_order<-sort(tab_sub, decreasing=T)
+  t<-tab_sub_order[1:3]/length(sub)
+  paste( round(t[3], digits=4))
+})
+
+output$tabla1namecat4Diciembre<-renderText({
+  
+  Subconjuntos<-na.omit(subset(catg_sin_rep_Diciembre,
+                               Diciembre.Categoriaf3==input$select2,
+                               select=c(Diciembre.CATEGORIA1)))
+  
+  sub<-Subconjuntos[,1]; tab_sub<-table(sub);
+  tab_sub_order<-sort(tab_sub, decreasing=T)
+  t<-tab_sub_order[1:4]/length(sub)
+  nam<-vector(mode="character")
+  for(i in 1:4){
+    nam[i]<-names(t[i])
+  }
+  paste( names(t[4]))
+})
+
+output$tabla1probcat4Diciembre<-renderText({
+  Subconjuntos<-na.omit(subset(catg_sin_rep_Diciembre,
+                               Diciembre.Categoriaf3==input$select2,
+                               select=c(Diciembre.CATEGORIA1)))
+  
+  sub<-Subconjuntos[,1]; tab_sub<-table(sub);
+  tab_sub_order<-sort(tab_sub, decreasing=T)
+  t<-tab_sub_order[1:4]/length(sub)
+  paste( round(t[4], digits=4))
+})
+
+output$tabla1namecat5Diciembre<-renderText({
+  Subconjuntos<-na.omit(subset(catg_sin_rep_Diciembre,
+                               Diciembre.Categoriaf3==input$select2,
+                               select=c(Diciembre.CATEGORIA1)))
+  
+  sub<-Subconjuntos[,1]; tab_sub<-table(sub);
+  tab_sub_order<-sort(tab_sub, decreasing=T)
+  t<-tab_sub_order[1:5]/length(sub)
+  nam<-vector(mode="character")
+  for(i in 1:5){
+    nam[i]<-names(t[i])
+  }
+  paste( names(t[5]))
+})
+
+output$tabla1probcat5Diciembre<-renderText({
+  Subconjuntos<-na.omit(subset(catg_sin_rep_Diciembre,
+                               Diciembre.Categoriaf3==input$select2,
+                               select=c(Diciembre.CATEGORIA1)))
+  
+  sub<-Subconjuntos[,1]; tab_sub<-table(sub);
+  tab_sub_order<-sort(tab_sub, decreasing=T)
+  t<-tab_sub_order[1:5]/length(sub)
+  paste(round(t[5], digits=4))
+})
+
+
+})
+
+
+#Diciembre
+catg_Diciembre<-data.frame(Diciembre$CATEGORIA1,
+                           Diciembre$Categoriaf3,
+                           Diciembre$SLSEQ, Diciembre$TICKET)
+
+catg_sin_rep_Diciembre<-unique(catg_Diciembre)
